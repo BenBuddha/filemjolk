879520f1c2d3c1bfb54a802150bbc2d42afd7ced
BenBuddha <ben.skoglund@live.se>
2022-02-13 10:24:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://innshare.herokuapp.com";
+const baseURL = "https://filemjolk-download.herokuapp.com/";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
